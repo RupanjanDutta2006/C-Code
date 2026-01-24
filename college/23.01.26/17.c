91d0b0f511b6ae0f0df9b184c42d6ce70d477e40
RupanjanDutta2006 <rupanjandutta06@gmail.com>
2026-01-24 19:58:22 +0530

    practicing

---
+++ college/23.01.26/17.c
@@ -0,0 +1,15 @@
+//Change lowe case unto upper case
+#include <stdio.h>
+void main()
+{
+    char str[100];
+    int i;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 32;
+    }
+    printf("Uppercase string: %s\n", str);
+}
