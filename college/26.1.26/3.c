5497d816bdfe3b3efe05514db9230540afdd5c74
RupanjanDutta2006 <rupanjandutta06@gmail.com>
2026-01-26 23:01:23 +0530

    practicing

---
+++ college/26.1.26/3.c
@@ -0,0 +1,52 @@
+//Stucture to find out the 2nd max
+#include<stdio.h>
+#include<string.h>
+struct Student
+{
+    int roll,marks;
+    char name[20];
+}arr[20];
+void main()
+{
+    int n,i,max,smax,k,x;
+    printf("Enter the Range =");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("Enter the Roll no = ");
+        scanf("%d",&arr[i].roll);
+        printf("Enter the Name = ");
+        scanf("%s",&arr[i].name);
+        printf("Enter the Marks = ");
+        scanf("%d",&arr[i].marks);
+    }
+    printf("------------------------------------------------------------------------\n");
+    printf("Roll\tName\tmarks\n");
+    printf("------------------------------------------------------------------------\n");
+    for(i=0;i<n;i++)
+        printf("%d\t%s\t%d\n",arr[i].roll,arr[i].name,arr[i].marks);
+    max=arr[0].marks;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i].marks>max)
+        {
+            max=arr[i].marks;
+            k=i;
+        }
+    }
+    smax=-1;
+    for(i=0;i<n;i++)
+    {
+        if(k!=i)
+        {
+            if(arr[i].marks>smax)
+            {
+                smax=arr[i].marks;
+                x=i;
+            }
+        }
+        else
+            continue;
+    }
+    printf("2nd max = %d Roll,%s Name,%d marks",arr[x].roll,arr[x].name,arr[x].marks);
+}
